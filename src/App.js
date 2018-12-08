f26e3034c9bc100d1508bd81331f8c58f647e95b
Alex Balkanskiy <balkanskiy.alexey@gmail.com>
2018-12-08 21:04:55 +0300

    Add stateFull component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import TodoListMin from "./TodoListMin";
+import IncidentDescription from "./IncidentDescription";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoListMin
+        <IncidentDescription
           incident={{
-            id: 236,
+            id: 246,
             description: "Сбой в работе сервера авторизации",
             source: "Сервер авторизации",
             date: "14.11.18"
---
+++ src/IncidentDescription.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+export default class IncidentDescription extends React.Component {
+  state = {
+    value: "",
+    copied: false
+  };
+
+  render() {
+    const { id, description, source, date } = this.props.incident;
+
+    return (
+      <div className="IncidentDescription">
+        <h2>Инцидент №{id}</h2>
+        <ul>
+          <li>Описание: {description}</li>
+          <li>Источник: {source}</li>
+          <li>Дата: {date}</li>
+        </ul>
+
+        <CopyToClipboard
+          text={description}
+          onCopy={(text) => this.setState({ copied: true })}
+        >
+          <button>Copy to clipboard with button</button>
+        </CopyToClipboard>
+
+        {this.state.copied ? <span>Copied.</span> : null}
+      </div>
+    );
+  }
+}
